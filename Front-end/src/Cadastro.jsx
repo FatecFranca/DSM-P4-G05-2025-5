720aba61ac9dfd9c9a5a4bf06dc799b917f587e8
RamonFranco <ramonccastro@hotmail.com>
2025-06-11 22:23:05 -0300

    fix: Ajustando para o ip da VM

--- Front-end/src/Cadastro.jsx
+++ Front-end/src/Cadastro.jsx
@@ -13,7 +13,7 @@ const handleCadastro = async (e) => {
   e.preventDefault();
   setLoading(true);
   try {
-    const response = await fetch('http://localhost:8080/auth/register', {
+    const response = await fetch('http://172.200.143.12/auth/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password }),
