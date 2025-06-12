f4e0a40328f40a30bb2f2d04f3d2ee40a7fcfc59
RamonFranco <ramonccastro@hotmail.com>
2025-06-12 13:50:42 -0300

    fix: cors ip host para VM

--- Mobile/airsensemob/api/axios.js
+++ Mobile/airsensemob/api/axios.js
@@ -1,16 +1,16 @@
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const api = axios.create({
-  baseURL: 'http://192.168.100.138:8080',
+  baseURL: "http://172.200.143.12:8080",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 api.interceptors.request.use(
   async (config) => {
-    const token = await AsyncStorage.getItem('token');
+    const token = await AsyncStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
